f7a6d83ad6def9406a1e995dca66ad7193c2e476
jwick4298 <jwick4298@gmail.com>
2020-10-19 10:19:14 +0530

    Completed. Guest can also comment on any blog post, but approval will be done by admin side.

--- demo/views.py
+++ demo/views.py
@@ -77,12 +77,12 @@ def add_comment_to_post(request, pk):
 
 @login_required
 def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk = pk)
-    comment.save()
-    return redirect('demo/post_detail.html',pk = comment.post.pk)
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('demo/post_detail.html', pk = comment.post.pk)
+    return redirect('post_detail', pk=comment.post.pk)
