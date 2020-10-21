3f6acfe2afd01055c85f588bddb5368ea7f63260
jwick4298 <jwick4298@gmail.com>
2020-10-21 09:56:52 +0530

    Some minor changes

--- demo/forms.py
+++ demo/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title','text','reference')
+        fields = ('category','title','text','reference')
 
 class CommentForm(forms.ModelForm):
     class Meta:
